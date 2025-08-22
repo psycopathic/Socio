f0e7d914b09f109b52d97f7007f6a62e5ad6b149
Harsh Shrivastava <harshsrivastava0302@gmail.com>
2025-08-22 23:42:36 +0530

    sidebar

---
+++ src/actions/user.action.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export async function syncUser() {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+
+    if (!userId || !user) return;
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+    });
+    if (existingUser) return existingUser;
+    // console.log("hi")
+    const dbUser = await prisma.user.create({
+      data: {
+        clerkId: userId,
+        name: `${user.firstName || ""} ${user.lastName || ""}`,
+        username:
+          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0], //nullable
+        email: user.emailAddresses[0].emailAddress,
+        image: user.imageUrl,
+      },
+    });
+    return dbUser;
+  } catch (error) {
+    console.log("Error in syncUser", error);
+  }
+}
+
+export async function getUserById(clerkId: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        clerkId,
+      },
+      include: {
+        _count: {
+          select: {
+            following: true,
+            followers: true,
+            post: true,
+          },
+        },
+      },
+    });
+    return user;
+  } catch (error) {
+    console.log("Error in getUserById", error);
+  }
+}
